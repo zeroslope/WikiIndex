90a1345ebe83f9eb8009f1d6d03580ac7441e22c
55242 <738543046@qq.com>
2018-12-16 20:58:07 +0800

    change the replace way but MLE
    - replaceAll("[^\\w-]", " ") 把所有不是字母、数字、下划线、汉字、减号的字符替换为空格，再split
    - change flatMap reduceByKey 在开始的时候把每个文档每个单词的出现的次数统计好

--- src/main/scala/WikiIndex/WordCount.scala
+++ src/main/scala/WikiIndex/WordCount.scala
@@ -4,22 +4,26 @@ import com.databricks.spark.xml.XmlInputFormat
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.spark.rdd.RDD._
 import org.apache.spark.{SparkConf, SparkContext}
-// import org.apache.log4j.{Level,Logger}
+import org.apache.log4j.{Level,Logger}
 import scala.Array.concat
 import scala.xml.XML
+import scala.collection.mutable.{Map, ArrayBuffer}
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    // Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    val conf = new SparkConf().setAppName("WikiIndex")
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    val conf = new SparkConf().setAppName("WikiIndex2")
     val sc = new SparkContext(conf)
 
     sc.hadoopConfiguration.set(XmlInputFormat.START_TAG_KEY, "<page>")
     sc.hadoopConfiguration.set(XmlInputFormat.END_TAG_KEY, "</page>")
     sc.hadoopConfiguration.set(XmlInputFormat.ENCODING_KEY, "utf-8")
 
+    val inputFile = "hdfs://10.141.200.205:9000/home/jc/enwikisource-20171020-pages-articles-multistream.xml"
+//    val inputFile = "hdfs://localhost:9000/wiki.xml"
+
     val records = sc.newAPIHadoopFile(
-      "hdfs://10.141.200.205:9000/home/jc/enwikisource-20171020-pages-articles-multistream.xml",
+      inputFile,
       classOf[XmlInputFormat],
       classOf[LongWritable],
       classOf[Text]
@@ -28,35 +32,46 @@ object WordCount {
         val docXml = XML.loadString(doc._2.toString())
         val id = (docXml \ "id").text
         val text = (docXml \ "revision" \ "text").text
+        val m:Map[String, (Int, ArrayBuffer[Int])] = Map()
 
         text.replaceAll("(&ensp;|&emsp;|&nbsp;)", " ")
           .replaceAll("&lt;", "<")
           .replaceAll("&gt;", ">")
           .replace("&amp;", "&")
           .replaceAll("&quot;", "\"")
+          .replaceAll("[^\\w-]", " ")
           .split("\\s")
           .zipWithIndex
-          .map {
-            case (word, index) => ((word, id), (Array(index), 1))
+          .foreach {
+            case (word, index) => {
+              if(m.contains(word)) {
+                val t = m(word)
+                m(word) = (t._1+1, (t._2) += index)
+              } else {
+                m(word) = (1, ArrayBuffer(index))
+              }
+            }
           }
+        m.map {
+          case (a, b) => {
+            (a, Array( (id, b._1, b._2.toArray )))
+          }
+        }
       }
-    }.reduceByKey {
-      case (a, b) => (concat(a._1,b._1), a._2+b._2)
-    }.map {
-      case((w, id),(pos, c)) => (w, Array( (id, c, pos) ))
     }.reduceByKey {
       case (a, b) => concat(a, b)
     }.map {
-      case(a, b) => (a, (b.length, b))
-    }.saveAsObjectFile("hdfs://10.141.200.205:9000/home/16307130023/wikiindex")
-      /*.foreach {
-      case (w, (df, b)) => {
-        print(s"word:\t${w}\tdf:\t${df}\n")
-        for (i <- b) {
-          print(s"(${i._1} ${i._2}) ")
-        }
-        println()
-      }}
-      */
+      case (a, b) => (a, (b.length, b))
+    }.saveAsObjectFile("hdfs://10.141.200.205:9000/home/16307130023/cleanindex")
+
+//      .foreach {
+//        case (w, (df, b)) => {
+//          print(s"word:\t${w}\tdf:\t${df}\n")
+//          for (i <- b) {
+//            print(s"(${i._1} ${i._2}) ")
+//          }
+//          println()
+//        }}
+
   }
 }
